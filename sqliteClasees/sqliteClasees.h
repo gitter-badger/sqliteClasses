334b4bbabfb1dab762c6b0f370b7cdcf0aff880a
osoftz <ios@osoftz.com>
2018-11-17 12:15:07 +0530

    Initial Commit

---
+++ sqliteClasees/sqliteClasees.h
@@ -0,0 +1,19 @@
+//
+//  sqliteClasees.h
+//  sqliteClasees
+//
+//  Created by Developers on 17/11/18.
+//  Copyright © 2018 osoftz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sqliteClasees.
+FOUNDATION_EXPORT double sqliteClaseesVersionNumber;
+
+//! Project version string for sqliteClasees.
+FOUNDATION_EXPORT const unsigned char sqliteClaseesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sqliteClasees/PublicHeader.h>
+
+
